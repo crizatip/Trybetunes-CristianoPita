88ce17245a14b5eb2167d25879a3a6399a7366c2
cristiano <crizatip@gmail.com>
2022-07-05 16:06:21 -0300

    req 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Albums from './pages/Albums';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Login>
+          <Link to="/"> Login </Link>
+        </Login>
+        <Search>
+          <Link to="/search"> Login </Link>
+        </Search>
+        <Albums>
+          <Link to="/album/:id"> Login </Link>
+        </Albums>
+        <Favorites>
+          <Link to="/favorites"> Login </Link>
+        </Favorites>
+        <Profile>
+          <Link to="/profile"> Login </Link>
+        </Profile>
+        <ProfileEdit>
+          <Link to="/profile/edit"> Login </Link>
+        </ProfileEdit>
+        <NotFound>
+          <Link to=" "> Login </Link>
+        </NotFound>
+      </BrowserRouter>);
   }
 }
 
