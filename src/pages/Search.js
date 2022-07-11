01c7fa4691403b92390e1819d6c0ec11fad69bb3
cristiano <crizatip@gmail.com>
2022-07-11 13:10:42 -0300

    req 5 finalizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import Header from './Header';
-import { getUser } from '../services/userAPI';
 
 class Search extends React.Component {
-  getUserHandler = async () => {
-    const existentUser = await getUser();
-    return existentUser;
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSearch: '',
+      buttonDisabled: true,
+    };
+  }
+
+  buttonDisabledHandle = () => {
+    const { currentSearch } = this.state;
+    if (currentSearch.length >= '2') {
+      this.setState({ buttonDisabled: false });
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState(
+      { currentSearch: event.target.value },
+      () => {
+        this.buttonDisabledHandle();
+      },
+    );
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
   render() {
+    const { currentSearch, buttonDisabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search"> </div>
+        <div data-testid="page-search">
+          <form id="formSearch" onSubmit={ this.handleSubmit }>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="search"
+              id="search"
+              onChange={ this.handleChange }
+              value={ currentSearch }
+            />
+            <button
+              type="submit"
+              form="formSearch"
+              data-testid="search-artist-button"
+              disabled={ buttonDisabled }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
